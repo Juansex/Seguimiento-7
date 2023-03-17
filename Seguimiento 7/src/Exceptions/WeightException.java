5f6b120d0d656402f75f4547f0b04e09d4df521a
Juan Sebastián Botero <114439930+Juansex@users.noreply.github.com>
2023-03-17 11:06:24 -0500

    Add files via upload

---
+++ Seguimiento 7/src/Exceptions/WeightException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+
+public class WeightException extends  Exception{
+    public WeightException(String message) {
+        super(message);
+    }
+}
---
+++ Seguimiento 7/test/IngredientTest.java
@@ -0,0 +1,64 @@
+import Exceptions.WeightException;
+import model.Ingredient;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IngredientTest {
+
+    private Ingredient ingredient;
+
+    public void setupStage1(){
+        try {
+            ingredient = new Ingredient("Tomate",245);
+        } catch (WeightException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void testAddWeight(){
+        setupStage1();
+        try {
+            ingredient.addWeight(54);
+            assertEquals(299, ingredient.getWeight());
+
+
+        } catch (WeightException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void testAddWeight2(){
+        setupStage1();
+        try {
+            ingredient.addWeight(-100);
+
+        } catch (WeightException e) {
+            System.out.println(e);
+            assertEquals(245, ingredient.getWeight());
+        }
+    }
+
+    public void testRemoveWeight(){
+        setupStage1();
+        try {
+            ingredient.removeWeight(45);
+            assertEquals(200, ingredient.getWeight());
+
+
+        } catch (WeightException e) {
+            System.out.println(e);
+        }
+    }
+    public void testRemoveWeight2(){
+        setupStage1();
+        try {
+            ingredient.removeWeight(-100);
+
+        } catch (WeightException e) {
+            System.out.println(e);
+            assertEquals(245, ingredient.getWeight());
+        }
+    }
+}
